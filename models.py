9d5d3d7c8826f0ef946dd97654381c074203c325
Luis Chavarriaga <luischa123@gmail.com>
2020-12-01 16:50:58 -0500

    add models file, update requirements
    este commit agrega el archivo donde se construirán y evaluarán los
    modelos, así como las dependencias de sklearn y pandas.

---
+++ models.py
@@ -0,0 +1,7 @@
+"""
+En este módulo se crean los modelos de clustering para los datos recolectados y se comparan.
+"""
+
+from sklearn.cluster import (KMeans, SpectralClustering,
+                             AgglomerativeClustering, OPTICS)
+
